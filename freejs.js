976d71d6c426a2e4dfa79c1fae9a081e0c34fcd3
ElisDN <mail@elisdn.ru>
2012-08-29 21:44:06 +0400

    Version 2.3.1

--- freejs.js
+++ freejs.js
@@ -611,7 +611,7 @@ if (typeof jQuery != 'undefined') {
                 return result;
             }
 
-            var scriptVersion = '2.3';
+            var scriptVersion = '2.3.1';
             var clientLastVersion = '2.0';
 
             /* Init */
